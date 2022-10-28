353e245f90692cbe3d76861f2715e3be0a808b66
sayambothra <bothrasayam007@gmail.com>
2022-10-28 18:29:51 +0530

    Netmeds Cucumber phase1

---
+++ src/test/java/Runner/TestRunner.java
@@ -0,0 +1,22 @@
+package Runner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		//To connect TestRunner and TestSteps class
+		features="Feature",
+		glue="StepDefinition")
+
+public class TestRunner {
+
+	
+
+}
